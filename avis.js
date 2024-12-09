47a5e33166a4da98ab24c7bb8e81edf9798508c8
JosueMbunga <118195236+Studiomulex@users.noreply.github.com>
2024-12-09 21:11:28 +0100

    mise en forme avec prettier

--- avis.js
+++ avis.js
@@ -1,10 +1,7 @@
-export function ajoutListenerAvis(){
-    const pieceElems=document.querySelectorAll(".fiches article button")
+export function ajoutListenerAvis() {
+  const pieceElems = document.querySelectorAll(".fiches article button");
 
-    for (let i = 0; i < pieceElems.length; i++) {
-        pieceElems[i].addEventListener("click",async function (event) {
-            
-        })
-        
-    }
+  for (let i = 0; i < pieceElems.length; i++) {
+    pieceElems[i].addEventListener("click", async function (event) {});
+  }
 }
