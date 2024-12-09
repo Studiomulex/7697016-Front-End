6ea36c0ddb60540ef89e8d2ae8f65bf9483702bf
JosueMbunga <118195236+Studiomulex@users.noreply.github.com>
2024-12-09 21:10:25 +0100

    ajout de code pour parcourir les avis a afficher

---
+++ avis.js
@@ -0,0 +1,10 @@
+export function ajoutListenerAvis(){
+    const pieceElems=document.querySelectorAll(".fiches article button")
+
+    for (let i = 0; i < pieceElems.length; i++) {
+        pieceElems[i].addEventListener("click",async function (event) {
+            
+        })
+        
+    }
+}
